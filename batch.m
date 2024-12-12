19d85db56f05836eb4927372035487ad027410f8
Maksym Naumenko <maksym0naumenko02@gmail.com>
2024-12-12 12:56:10 +0100

    updated batch

--- batch.m
+++ batch.m
@@ -1,16 +1,17 @@
 function batch
     input_dir = 'images/reduced_size/pianta3g';
-    images = dir(input_dir);
-    num_images = length(images)-2;
+    images = dir(fullfile(input_dir, '*.jpg'));
+    images = images(~[images.isdir]);
     
-    outs = zeros(300, 400, num_images-2);
+    num_images = length(images);
+    outs = zeros(300, 400, num_images);
     num_superpixels = 200;
     
     tic;
-    for k=3:num_images+2
+    for k=1:num_images
         im = imread(fullfile(input_dir, images(k).name));
         desc = extract_slic_descriptors(im, num_superpixels);
-        outs(:,:,k-2) = slic_kmeans(im, desc, 2, false, false);
+        outs(:,:,k) = slic_kmeans(im, desc, 2, false, false);
     end
     toc;
     
@@ -18,6 +19,6 @@ function batch
     set(t, 'WindowState', 'maximized');
     
     for k=1:num_images
-       subplot(5,4,k); imagesc(outs(:,:,k)); colormap(jet); axis image; axis off; title(images(k+2).name);
+       subplot(5,4,k); imagesc(outs(:,:,k)); colormap(jet); axis image; axis off; title(images(k).name);
     end
 end
