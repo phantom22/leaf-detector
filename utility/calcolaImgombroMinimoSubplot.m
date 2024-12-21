f0efdcc68956bd57912f02132e155c80cbd2bed8
LorenzoPina-dev <67581426+LorenzoPina-dev@users.noreply.github.com>
2024-12-21 19:29:58 +0100

    aggiunta ground truth,immagini senza sfondo e crazione algoritmo per testare l'accuratezza della segmentazione

--- utility/calcolaImgombroMinimoSubplot.m
+++
@@ -1,8 +0,0 @@
-function [righe,colonne]= calcolaImgombroMinimoSubplot(numImage)
-    lato=ceil(sqrt(numImage));
-    righe=lato;
-    colonne=lato;
-    if((righe-1)*colonne>=numImage)
-        righe=righe-1;
-    end
-end
