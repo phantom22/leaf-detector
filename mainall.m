c5427da8de2050cee5065662f8b425a04856d31b
phantom22 <maksym0naumenko02@gmail.com>
2025-01-24 20:48:16 +0100

    classifier accuracy

--- mainall.m
+++ mainall.m
@@ -14,6 +14,12 @@ function mainall
         "A","A","A",...
         "B","B","B",...
         "C","C","C"};
+    
+    gt_counts = [5 4 5 5 5 5 6 5 7 6 6 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 5 5 4 5 5 5 5 5 6];
+
+    mapping = containers.Map(["A","B","C","D","E","F","G","H","I","L","M","N"], [1 2 3 4 5 6 7 8 9 10 11 12]);
+
+    total_num_leafs = sum(gt_counts);
 
     [num_images, class_full_paths, ~] = image_paths_from_dir("images/Z");
 
@@ -22,20 +28,28 @@ function mainall
     dummy_se = strel('disk', 0);
     se = strel('disk', 5);
 
-    figure_maximized;
+    correct_guesses = 0;
 
+    figure_maximized;
     for i=1:num_images
         im = im2double(imread(class_full_paths{i}));
 
-        f = sqrt(size(im,1) * size(im,2) / (120000));
+        f = sqrt(size(im,1) * size(im,2) / 120000);
 
         im = imresize(im, size(im,1:2) / f);
 
         mask = segment(im, se);
-        classificato = classify(im, mask, dummy_se);
+        [classificato,counts] = classify(im, mask, dummy_se);
+
+
+        class_label = gt{i};
+        expected_class = mapping(class_label);
+        correct_guesses = correct_guesses + counts(expected_class);
 
         tsubplot(m,n,i);
         visualize_classification(classificato);
-        title(gt(i));
+        title(class_label);
     end
+
+    fprintf("Accuracy: %.2f%%\n", correct_guesses / total_num_leafs * 100);
 end
