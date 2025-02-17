208a195052701d2ddf884f19b5604b04b2cfd44e
phantom22 <maksym0naumenko02@gmail.com>
2025-02-17 22:00:19 +0100

    prepare_classification_data/normalize_region_descriptors

--- classification/normalize_region_descriptors.m
+++ classification/normalize_region_descriptors.m
@@ -11,11 +11,28 @@ function [data,min_bounds,max_bounds] = normalize_region_descriptors(data, for_t
         return;
     end
 
-    cropped = data(:,1:end-1);
+    num_features = size(data,2) - 1;
+
+    cropped = data(:,1:num_features);
     min_bounds = min(cropped);
     max_bounds = max(cropped);
-
+   
     l = length(ignore_columns);
+
+    if nnz(isnan(cropped)) ~= 0
+        for i=1:num_features
+            count = nnz(isnan(cropped(:,i)));
+            if count ~= 0
+                fprintf("The feature #%d has %d NaN entries!\n", i, count);
+            end
+        end
+    end
+
+    if l > num_features
+        fprintf("ignore_columns exceed the actual count of features!\n");
+        l = num_features;
+    end
+
     for i=1:l
         cind = ignore_columns(i);
         min_bounds(cind) = 0;
--- prepare_data/prepare_classification_data.m
+++ prepare_data/prepare_classification_data.m
@@ -18,7 +18,7 @@ function [data,min_bounds,max_bounds] = prepare_classification_data(noise_streng
     
     tot_num_images = sum(class_num_images);
 
-    se = strel('disk', 5);
+    se = strel('disk', 3);
 
     pos = 1;
 
@@ -28,17 +28,17 @@ function [data,min_bounds,max_bounds] = prepare_classification_data(noise_streng
         for k=1:num_images
             if num_classes == 1
                 im_path = class_full_paths{k};
-                %gt_path = gt_full_paths{k};
+                % gt_path = gt_full_paths{k};
             else
                 im_path = class_full_paths{i}{k};
-                %gt_path = gt_full_paths{i}{k};
+                % gt_path = gt_full_paths{i}{k};
             end
 
             im = im2single(imread(im_path));
 
             %gt = bwopen(bwclose(imread(gt_path) > 1, se), se);
+          
             %K = gt;
-
             K = segment(im, se);
 
             [labeledImage, numRegions] = bwlabel(K);
@@ -49,10 +49,12 @@ function [data,min_bounds,max_bounds] = prepare_classification_data(noise_streng
                 K = (labeledImage == maxIndex);
             end
 
+            im = whitebalance(im);
+
             if pos == 1
                 v = region_descriptors(im, K);
                 num_features = size(v, 1);
-                data = zeros(tot_num_images, num_features + 1);
+                data = zeros(tot_num_images, num_features + 1, 'single');
                 data(pos, 1:num_features) = v;
                 data(pos, num_features + 1) = i;
             else
@@ -67,10 +69,12 @@ function [data,min_bounds,max_bounds] = prepare_classification_data(noise_streng
 
     data_without_labels = data(:,1:end-1);
 
-    data_avg = sum(data_without_labels) / size(data,1);
-    noise = ((single(-1) + single(2)*rand(size(data,1), size(data,2)-1,'single')) .* noise_strength) .* data_avg;
+    if noise_strength ~= 0
+        data_avg = sum(data_without_labels) / size(data,1);
+        noise = ((single(-1) + single(2)*rand(size(data,1), size(data,2)-1,'single')) .* noise_strength) .* data_avg;
+    
+        data(:,1:end-1) = data_without_labels + noise;
+    end
 
-    data(:,1:end-1) = data_without_labels + noise;
-
-    [data,min_bounds,max_bounds] = normalize_region_descriptors(data, true, 1:7);
+    [data,min_bounds,max_bounds] = normalize_region_descriptors(data, true, []);
 end
