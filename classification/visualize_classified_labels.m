f45c960146323088cbeb0c9c6bc19db3a152b669
LorenzoPina-dev <67581426+LorenzoPina-dev@users.noreply.github.com>
2025-02-22 19:16:54 +0100

    mostra nomi foglie al posto di lettere

--- classification/visualize_classified_labels.m
+++ classification/visualize_classified_labels.m
@@ -1,13 +1,15 @@
 function visualize_classified_labels(classificato)
     class_folders = ["A","B","C","D","E","F","G","H","I","L","M","N","Unknown"];
+    mappingNome = ["Lauroceraso","Edera","Laurotino","Rubus","Ulivo","Acero riccio","Bambù","Olmo","Agrifoglio","Rovere","Acero campestre","Pungitopo","Unknown"];
+    
     [labels, numRegions] = bwlabel(classificato~=0);
     for i=1:numRegions
         maskF = uint8(labels==i);
         c = regionprops(maskF,"Centroid").Centroid;
         classe = max(max(maskF.*classificato));
-        letteraClasse = class_folders(classe);
+        letteraClasse =mappingNome( classe);
         hold on;
-        text(c(1), c(2), letteraClasse, 'Color', hex2rgb('#FFFF99'), 'FontSize', 8, 'FontWeight', 'bold');
+        text(c(1)-strlength(letteraClasse)*7, c(2), letteraClasse, 'Color', hex2rgb('#FFFF99'), 'FontSize', 8, 'FontWeight', 'bold');
         hold off;
     end
 end
